d179bd1bf4b5236bf00baf47be6bd9a78c6126e9
Robert Biggs <rbiggs@ymail.com>
2019-12-14 09:51:29 -0800

    Updated types.
    1. Updated version of TypeScript.
    2. Autogenerated d.ts file from JSDoc comments.

---
+++ types/index.d.ts
@@ -0,0 +1,6 @@
+/**
+ * Function to create an RFC4122 version 4 compliant uuid. If a truthy value is passed as an argument, it will return a uuid without hyphens.
+ * @param {boolean} [bool] True or false.
+ * @return {string} string
+ */
+export function uuid(bool?: boolean): string;
